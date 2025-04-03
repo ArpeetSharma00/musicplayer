5901ecd92a9f64cb2cce7f9903472ab082bfb67a
Arpeet Sharma <arpeet047@gmail.com>
2025-04-03 17:41:46 +0530

    Create user_login.php

---
+++ user_login.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include 'db_connect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) == 1) {
+        $_SESSION['username'] = $username;
+        header("Location: user_dashboard.php");
+    } else {
+        echo "Invalid username or password";
+    }
+}
+?>
